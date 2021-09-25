006120e18a1d64acc470ad371b4f436d9891e648
Mahmoud Osman <34916796+MahmoudAhmadOsman@users.noreply.github.com>
2021-09-24 19:43:02 -0500

    Update Post.model.js

--- Model/Post.model.js
+++ Model/Post.model.js
@@ -1,32 +1,24 @@
 const mongoose = require('mongoose');
 const post = mongoose.Schema;
 
-
 let postSchema = new mongoose.Schema({
 	title:{
 		type:String,
 		require: true
 	},
-
 	body:{
 		type:String,
 		require: true
 	},
-
 	author:{
 		type:String
 	},
-
-
 	url:{
 		type:String
 	},
-
 	date:{
 		type: Date, default: Date.now
-
 	}
-
 });
 
 //export this modal so that you can use it on other files
