a35f61fa4565621e7b72000019dc8c06efa1cbbd
Mahmoud Osman <34916796+MahmoudAhmadOsman@users.noreply.github.com>
2022-02-10 10:44:16 -0600

    Update db.createCollection.js

--- db.createCollection.js
+++ db.createCollection.js
@@ -1,25 +1,18 @@
-
-
-// Create Collection or Table in SQL
-
-//db.createCollection('posts')
-
 db.posts.insert([
 {
   
    title: 'Post One', 
    body: 'This is post one',
    author: 'Mahmoud Osman',
-   url: 'http://www.netsansoftware.com',
-   
-},
+   url: 'https://www.mahmoudosman.com',
 
+},
 {
   
    title: 'Post Two', 
    body: 'This is post two',
    author: 'Mahmoud Osman',
-   url: 'http://www.netsansoftware.com',
+   url: 'https://mahmoudosman.com',
    
 }
 ])
